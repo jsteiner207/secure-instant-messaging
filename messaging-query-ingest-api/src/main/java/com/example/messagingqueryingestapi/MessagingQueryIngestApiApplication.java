cb2ca66008874add55fbdd1db5c791681d0fcf93
Jacob <jsteiner207@gmail.com>
2022-01-08 20:48:11 -0600

    added restful controller

--- messaging-query-ingest-api/src/main/java/com/example/messagingqueryingestapi/MessagingQueryIngestApiApplication.java
+++ messaging-query-ingest-api/src/main/java/com/example/messagingqueryingestapi/MessagingQueryIngestApiApplication.java
@@ -1,13 +1,24 @@
 package com.example.messagingqueryingestapi;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MessagingQueryIngestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingQueryIngestApiApplication.class, args);
 	}
+
+	@GetMapping
+	public List<String> hello() {
+		return List.of("Hello World");
+	}
+
 
 }
