68e56ed14aa1091a1a5e2b2417dfd8cc7102635b
Jacob <jsteiner207@gmail.com>
2022-01-08 19:18:11 -0600

    ok hopefully now it is commited

---
+++ messaging-query-ingest-api/src/test/java/com/example/messagingqueryingestapi/MessagingQueryIngestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messagingqueryingestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingQueryIngestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
